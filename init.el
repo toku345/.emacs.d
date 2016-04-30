fc0b7981b35b790ecfecfbfb743f81ea253ab866
Fumitaka Tokumitsu <fumitaka.tokumitsu@crowdworks.co.jp>
2016-04-30 18:11:42 +0900

    enable use-package

--- init.el
+++ init.el
@@ -1,10 +1,11 @@
 ;; -*- mode: Emacs-Lisp ; Coding: utf-8 -*-
 (set-language-environment "UTF-8")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;;
 ;;;
 ;;; setup package.el
 ;;;
+
 (require 'package)
 ;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
@@ -15,6 +16,20 @@
 
 (when (not (require 'use-package nil t))
   (package-install 'use-package))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;;
+;;;
+;;; basic configurations
+;;;
+
+;;; enable use-package
+(require 'use-package)
+(setq use-package-always-ensure t)
+(setq use-package-always-pin "melpa-stable")
+(setq use-package-verbose t)
+
+
 
 ;; 入力されるキーシーケンスを置き換える
 ;; ?\C-?はDELのキーシーケンス
