85abb59e601e19e72da8aaa7c7b729841994d20e
Fumitaka Tokumitsu <fumitaka.tokumitsu@crowdworks.co.jp>
2016-04-30 18:01:55 +0900

    Add package.el setting

--- init.el
+++ init.el
@@ -1,6 +1,21 @@
 ;; -*- mode: Emacs-Lisp ; Coding: utf-8 -*-
 (set-language-environment "UTF-8")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;
+;;;
+;;; setup package.el
+;;;
+(require 'package)
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+
+(when (not (require 'use-package nil t))
+  (package-install 'use-package))
+
 ;; 入力されるキーシーケンスを置き換える
 ;; ?\C-?はDELのキーシーケンス
 (keyboard-translate ?\C-h ?\C-?)
