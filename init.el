310dcfe8620f521ea26de6df33a3b8d57a464ee4
Fumitaka Tokumitsu <fumitaka.tokumitsu@crowdworks.co.jp>
2016-05-02 16:19:36 +0900

    confilict elscreen, remove header-line configurations　

--- init.el
+++ init.el
@@ -197,27 +197,6 @@
    global-mode-string
    ))
 
-(setq-default
- header-line-format
- '(""
-   (:propertize (:eval (shorten-directory default-directory 30))
-                face mode-line-folder-face)
-   (:propertize "%b"
-                face mode-line-filename-face)))
-
-(defun shorten-directory (dir max-length)
-  "Show up to `max-length` characters of a directory name `dir`."
-  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
-        (output ""))
-    (when (and path (equal "" (car path)))
-      (setq path (cdr path)))
-    (while (and path (< (length output) (- max-length 4)))
-      (setq output (concat (car path) "/" output))
-      (setq path (cdr path)))
-    (when path
-      (setq output (concat ".../" output)))
-    output))
-
 ;;; show line nums & word count in region when the range specification
 (add-to-list 'default-mode-line-format
              '(:eval (count-lines-and-chars)))
