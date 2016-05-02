f79432f6b9ab9f8aeac4b86530177dfb74f1f2d7
Fumitaka Tokumitsu <fumitaka.tokumitsu@crowdworks.co.jp>
2016-05-02 14:51:05 +0900

    add region info

--- init.el
+++ init.el
@@ -218,6 +218,20 @@
       (setq output (concat ".../" output)))
     output))
 
+;;; show line nums & word count in region when the range specification
+(add-to-list 'default-mode-line-format
+             '(:eval (count-lines-and-chars)))
+
+;; http://d.hatena.ne.jp/sonota88/20110224/1298557375
+(defun count-lines-and-chars ()
+  (if mark-active
+      (format "%d lines,%d chars "
+              (count-lines (region-beginning) (region-end))
+              (- (region-end) (region-beginning)))
+    ;; これだとエコーエリアがチラつく
+    ;; (count-lines-region (region-beginning) (region-end))
+    ""))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;;
 ;;;
 ;;; helm
