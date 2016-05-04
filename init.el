7cde7d2ee81e461304ec2615fc345713fdbaa5cb
Fumitaka Tokumitsu <fumitaka.tokumitsu@crowdworks.co.jp>
2016-05-04 23:34:34 +0900

    add elisp-mode configurations

--- init.el
+++ init.el
@@ -247,3 +247,38 @@
 (bind-keys :map helm-find-files-map
            ("C-h" . delete-backward-char)
            ("TAB" . helm-execute-persistent-action))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;;
+;;;
+;;; lisp
+;;;
+
+(use-package paredit
+  :defer t
+  :config
+  (bind-keys :map paredit-mode-map
+             ("C-h" . paredit-backward-delete))
+
+  (defun conditionally-enable-paredit-mode ()
+    (if (eq this-command 'eval-expression)
+        (paredit-mode 1)))
+  (add-hook 'minibuffer-setup-hook 'conditionally-enable-paredit-mode))
+
+(use-package eldoc
+  :defer t
+  :config
+  (setq eldoc-idle-delay 0.1
+        eldoc-minor-mode-string ""))
+
+(use-package rainbow-delimiters
+  :defer t)
+
+(defun my/lisp-mode-defaults ()
+  (paredit-mode 1)
+  (rainbow-delimiters-mode 1)
+  (eldoc-mode 1))
+
+(defun my/lisp-mode-hook ()
+  (my/lisp-mode-defaults))
+
+(add-hook 'emacs-lisp-mode-hook 'my/lisp-mode-hook)
