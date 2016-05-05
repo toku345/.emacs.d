ac13ec2905dc391893c1e728998462b4a8f2a5e2
Fumitaka Tokumitsu <toku345@gmail.com>
2016-05-05 23:28:33 +0900

    change avy-migemo configurations position

--- init.el
+++ init.el
@@ -256,16 +256,16 @@
   :config
   (ivy-mode 1))
 
-(use-package avy-migemo
-  :config
-  (avy-migemo-mode 1)
-  (require 'avy-migemo-e.g.swiper))
-
 (use-package swiper
   :bind (("C-s" . swiper)
          ("C-c C-r" . ivy-resume))
   :init
   (setq ivy-use-virtual-buffers t))
+
+(use-package avy-migemo
+  :config
+  (avy-migemo-mode 1)
+  (require 'avy-migemo-e.g.swiper))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;;
 ;;;
