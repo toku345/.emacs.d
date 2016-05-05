80c1a19c37eb3cfbeebaf6d186eaeaab39e3f3a9
Fumitaka Tokumitsu <fumitaka.tokumitsu@crowdworks.co.jp>
2016-05-05 14:42:21 +0900

    add clojure-mode configurations

--- init.el
+++ init.el
@@ -282,3 +282,36 @@
   (my/lisp-mode-defaults))
 
 (add-hook 'emacs-lisp-mode-hook 'my/lisp-mode-hook)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;:;;;;;;;;;;;;;
+;;;
+;;; clojure
+;;;
+
+(use-package clojure-mode
+  :init
+  (add-hook 'clojure-mode-hook #'yas-minor-mode)
+  (add-hook 'clojure-mode-hook #'subword-mode)
+  (add-hook 'clojure-mode-hook #'my/lisp-mode-hook))
+
+(use-package cider
+  :init
+  (add-hook 'cider-mode-hook #'clj-refactor-mode)
+  (add-hook 'cider-mode-hook #'company-mode)
+  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-repl-mode-hook #'my/lisp-mode-hook)
+  :diminish subword-mode
+  :config
+  (setq nrepl-log-messages t
+        cider-repl-display-in-current-window t
+        cider-repl-use-clojure-font-lock t
+        cider-prompt-save-file-on-load 'always-save
+        cider-font-lock-dynamically '(macro core function var)
+        cider-overlays-use-font-lock t)
+  (cider-repl-toggle-pretty-printing))
+
+(use-package cider-eval-sexp-fu)
+
+(use-package clj-refactor
+  :diminish clj-refactor-mode
+  :config (cljr-add-keybindings-with-prefix "C-c j"))
