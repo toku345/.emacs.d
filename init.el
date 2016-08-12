a81ab7a1d51456fcb56aadfd18d8c8e273e2546b
Fumitaka Tokumitsu <fumitaka.tokumitsu@crowdworks.co.jp>
2016-08-12 17:19:55 +0900

    Add rust configurations
    Need [racer](https://github.com/phildawes/racer) installation!!!

--- init.el
+++ init.el
@@ -554,6 +554,21 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; rust
+;;;
+(use-package rust-mode)
+
+(use-package racer
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode)
+  (bind-keys :map rust-mode-map
+             ("TAB" . company-indent-or-complete-common))
+  (setq company-tooltip-align-annotations t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; backup & auto saving
 ;;;
 ;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
