9a723676ec30d10a20e5d42e3481f47618cd1cf4
Fumitaka Tokumitsu <toku345@gmail.com>
2017-06-25 12:16:03 +0900

    Update http -> https : melpa & melpa-stable URL

--- init.el
+++ init.el
@@ -11,8 +11,8 @@
 ;;;
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/") t)
 
