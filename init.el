1a4fdfba1157e7b201d39655c8cdf96a971fb7d8
Fumitaka Tokumitsu <toku345@gmail.com>
2017-08-22 09:54:25 +0900

    Update markdown configurations

--- init.el
+++ init.el
@@ -611,7 +611,13 @@
 ;;;
 ;;; markdown
 ;;;
-(use-package markdown-mode)
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
