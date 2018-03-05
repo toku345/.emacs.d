2ac98027b1aae240580de6d9d02831c932805750
Fumitaka Tokumitsu <toku345@gmail.com>
2018-03-05 14:03:22 +0900

    cider-prompt-save-file-on-load → cider-save-file-on-load

--- init.el
+++ init.el
@@ -538,7 +538,7 @@
   (setq nrepl-log-messages t
         cider-repl-display-in-current-window t
         cider-repl-use-clojure-font-lock t
-        cider-prompt-save-file-on-load 'always-save
+        cider-save-file-on-load 'always-save
         cider-font-lock-dynamically '(macro core function var)
         cider-overlays-use-font-lock t)
   (cider-repl-toggle-pretty-printing)
