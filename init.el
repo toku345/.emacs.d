58d106ba3f51834cbabe8179638163b03988e78d
Fumitaka Tokumitsu <toku345@gmail.com>
2018-05-03 11:30:39 +0900

    Remove marmalade-repo

--- init.el
+++ init.el
@@ -13,7 +13,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/") t)
 
 (package-initialize)
