e9ed97e08555e36f6e2ed161b8f926d2c628808d
Fumitaka Tokumitsu <toku345@gmail.com>
2019-01-17 21:35:40 +0900

    Update rust-mode configurations

--- init.el
+++ init.el
@@ -794,6 +794,7 @@
 (use-package racer
   :config
   (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'rust-mode-hook #'smartparens-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode)
   (add-hook 'racer-mode-hook #'company-mode)
   (bind-keys :map rust-mode-map
@@ -807,8 +808,9 @@
 
 (use-package flycheck-rust
   :pin melpa
-  :config
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+  :init
+  (with-eval-after-load 'rust-mode
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
